6d2c579919abb605eac4218d2c9865d4286c3006
Gustavo Rutiquewiski <gustavo.rutiquewiski@outlook.com>
2024-03-14 14:32:44 -0300

    Fix

--- src/pages/Appointments/AppointmentsPage.tsx
+++ src/pages/Appointments/AppointmentsPage.tsx
@@ -1,5 +1,21 @@
+import { Container } from "@chakra-ui/react";
+
 function AppointmentsPage() {
-  return <></>;
+  return (
+    <>
+      <Container
+        display="flex"
+        flexDirection="row"
+        justifyContent="center"
+        alignItems="center"
+        maxW="100vw"
+        w="95vw"
+        h="90vh"
+      >
+        Appointments
+      </Container>
+    </>
+  );
 }
 
 export default AppointmentsPage;
--- src/pages/professionals/ProfessionalsPage.tsx
+++ src/pages/professionals/ProfessionalsPage.tsx
@@ -1,5 +1,21 @@
+import { Container } from "@chakra-ui/react";
+
 function ProfessionalsPage() {
-  return <></>;
+  return (
+    <>
+      <Container
+        display="flex"
+        flexDirection="row"
+        justifyContent="center"
+        alignItems="center"
+        maxW="100vw"
+        w="95vw"
+        h="90vh"
+      >
+        Professionals
+      </Container>
+    </>
+  );
 }
 
 export default ProfessionalsPage;
